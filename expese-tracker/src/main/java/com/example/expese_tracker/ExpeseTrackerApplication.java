fca7cbf0cce4c73a26ffa140eb1dcbd516e7eeab
Franklin Marcelin <55675901+franklinm54@users.noreply.github.com>
2026-06-06 15:56:45 -0400

    [Add] new files
    after config my initial setup in https://start.spring.io/
    I copy the the unzip file in my main directory

---
+++ expese-tracker/src/main/java/com/example/expese_tracker/ExpeseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.expese_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpeseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpeseTrackerApplication.class, args);
+	}
+
+}
---
+++ expese-tracker/src/test/java/com/example/expese_tracker/ExpeseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expese_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpeseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
